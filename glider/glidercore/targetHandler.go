b827d97b1785642dee40297671325b715679fb12
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2020-02-23 15:56:25 +0530

    Add method for removing backend

--- glider/glidercore/targetHandler.go
+++ glider/glidercore/targetHandler.go
@@ -3,6 +3,7 @@ package glidercore
 import (
 	"github.com/djmgit/go-iptables/iptables"
 	"paraglider/glider/models"
+	"time"
 )
 
 var iptableConstants = models.IptableConstantsHolder{
@@ -32,6 +33,30 @@ func CreateTargetForLb(TargetBackend models.TargetBackendHolder) error {
 
 		_ = ipt.Delete(iptableConstants.Nat, iptableConstants.Prerouting, "-p", iptableConstants.Tcp, "-d", TargetBackend.LbIp, "--dport", string(TargetBackend.LbPort), "-j", iptableConstants.Dnat, "--to-destination", TargetBackend.BackendIP + ":" + string(TargetBackend.BackendPort))
 
+		return err
+	}
+
+	return nil
+}
+
+func RemoveTargetForLb(TargetBackend models.TargetBackendHolder) error {
+
+	ipt, err := iptables.New()
+	if err != nil {
+		return err
+	}
+
+	err = ipt.AppendUnique(iptableConstants.Nat, iptableConstants.Postrouting, "-p", iptableConstants.Tcp, "-d", TargetBackend.BackendIP, "--dport", string(TargetBackend.BackendPort), "-j", iptableConstants.Snat, "--to-source", TargetBackend.LbPrivateIP)
+
+	if err != nil {
+		return  err
+	}
+
+	time.Sleep(5 * time.Second)
+
+	err = ipt.AppendUnique(iptableConstants.Nat, iptableConstants.Postrouting, "-p", iptableConstants.Tcp, "-d", TargetBackend.BackendIP, "--dport", string(TargetBackend.BackendPort), "-j", iptableConstants.Snat, "--to-source", TargetBackend.LbPrivateIP)
+
+	if err != nil {
 		return err
 	}
 
