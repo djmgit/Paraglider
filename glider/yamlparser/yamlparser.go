ed7acd52e829850b0fd2e341349bf338ce0584df
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2020-02-28 00:29:09 +0530

    Read yaml

--- glider/yamlparser/yamlparser.go
+++ glider/yamlparser/yamlparser.go
@@ -1,1 +1,23 @@
-import "gopkg.in/yaml"
+package yamlparser
+
+import (
+	"paraglider/glider/models"
+	"io/ioutil"
+)
+
+func ParseYaml(yamlPath string) (*models.Config, error) {
+	data, err := ioutil.ReadFile(yamlPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	config := models.Config{}
+	err = config.Parse(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
