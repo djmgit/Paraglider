e77b76bdadd500014676865dee196ad1158aedc9
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2020-04-03 00:08:31 +0530

    Add inline docs

--- glider/glidercore/targetHandler.go
+++ glider/glidercore/targetHandler.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 )
 
+// Struct containing list of constants for iptable rules
 var iptableConstants = models.IptableConstantsHolder{
 	Prerouting: "PREROUTING",
 	Postrouting: "POSTROUTING",
@@ -23,6 +24,7 @@ func CreateTargetForLb(TargetBackend models.TargetBackendHolder, roundRobinTurn 
 		return err
 	}
 
+	// Add the PREROUTING rule for the given backend
 	if roundRobinTurn != 1 {
 		err = ipt.AppendUnique(iptableConstants.Nat, iptableConstants.Prerouting, "-p", iptableConstants.Tcp, "-d", TargetBackend.LbIP, "--dport", strconv.Itoa(TargetBackend.LbPort), "-m", "statistic", "--mode", "nth", "--every", strconv.Itoa(roundRobinTurn), "--packet", "0", "-j", iptableConstants.Dnat, "--to-destination", TargetBackend.BackendIP + ":" + strconv.Itoa(TargetBackend.BackendPort))
 	} else {
@@ -33,6 +35,7 @@ func CreateTargetForLb(TargetBackend models.TargetBackendHolder, roundRobinTurn 
 		return err
 	}
 
+	// Add POSTROUTING rule for the given target only after PREROUTING rule has been added
 	err = ipt.AppendUnique(iptableConstants.Nat, iptableConstants.Postrouting, "-p", iptableConstants.Tcp, "-d", TargetBackend.BackendIP, "--dport", strconv.Itoa(TargetBackend.BackendPort), "-j", iptableConstants.Snat, "--to-source", TargetBackend.LbPrivateIP)
 
 	if err != nil {
@@ -56,6 +59,7 @@ func RemoveTargetForLb(TargetBackend models.TargetBackendHolder, roundRobinTurn 
 		return err
 	}
 
+	// Remove the PREROUTING rule
 	if roundRobinTurn != 1 {
 		err = ipt.Delete(iptableConstants.Nat, iptableConstants.Prerouting, "-p", iptableConstants.Tcp, "-d", TargetBackend.LbIP, "--dport", strconv.Itoa(TargetBackend.LbPort), "-m", "statistic", "--mode", "nth", "--every", strconv.Itoa(roundRobinTurn), "--packet", "0", "-j", iptableConstants.Dnat, "--to-destination", TargetBackend.BackendIP + ":" + strconv.Itoa(TargetBackend.BackendPort))
 	} else {
@@ -66,8 +70,7 @@ func RemoveTargetForLb(TargetBackend models.TargetBackendHolder, roundRobinTurn 
 		return  err
 	}
 
-	time.Sleep(5 * time.Second)
-
+	// Delete the coresponding POSTROUTING rule
 	err = ipt.Delete(iptableConstants.Nat, iptableConstants.Postrouting, "-p", iptableConstants.Tcp, "-d", TargetBackend.BackendIP, "--dport", strconv.Itoa(TargetBackend.BackendPort), "-j", iptableConstants.Snat, "--to-source", TargetBackend.LbPrivateIP)
 
 	if err != nil {
